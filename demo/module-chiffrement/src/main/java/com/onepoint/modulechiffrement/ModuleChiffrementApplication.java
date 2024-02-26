01509432f2e93fd5cca6cdc43b91a9e1064c1977
Ivan Béthus <i.bethus@groupeonepoint.com>
2024-02-26 17:17:39 +0100

    PERF init module chiffrement

---
+++ demo/module-chiffrement/src/main/java/com/onepoint/modulechiffrement/ModuleChiffrementApplication.java
@@ -0,0 +1,13 @@
+package com.onepoint.modulechiffrement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleChiffrementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuleChiffrementApplication.class, args);
+	}
+
+}
