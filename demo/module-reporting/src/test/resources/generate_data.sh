397fe17e09f335d92d4a5e61ed11466cca3ae80f
Ivan Béthus <i.bethus@groupeonepoint.com>
2024-06-18 18:18:48 +0200

    màj bdd

--- demo/module-reporting/src/test/resources/generate_data.sh
+++ demo/module-reporting/src/test/resources/generate_data.sh
@@ -11,17 +11,17 @@ generate_random_text() {
 
 # Définition des valeurs possibles pour les champs
 codes_postaux=('33300' '24002' '12003' '45004' '75005' '65006' '75007' '75008' '75009' '75010')
-pays=('France' 'Germany' 'United Kingdom' 'Italy' 'Spain' 'Portugal' 'Netherlands' 'Belgium' 'Switzerland' 'Austria')
+listePays=('France' 'Germany' 'United Kingdom' 'Italy' 'Spain' 'Portugal' 'Netherlands' 'Belgium' 'Switzerland' 'Austria')
 noms_destinataires=('Forrest Gump' 'Woody' 'Captain John Miller' 'Chuck Noland' 'Walt Disney' 'Paul Edgecomb' 'Joe Fox' 'Ben Bradlee' 'Jim Lovell' 'Charlie Wilson' 'Josh Baskin' 'James Donovan' 'Andrew Beckett' 'Larry Crowne' 'David S. Pumpkins' 'Michael Sullivan' 'Richard Phillips' 'Joe Banks' 'James Mattingly' 'Carl Hanratty')
 noms_expediteurs=('Forrest Gump' 'Woody' 'Captain John Miller' 'Chuck Noland' 'Walt Disney' 'Paul Edgecomb' 'Joe Fox' 'Ben Bradlee' 'Jim Lovell' 'Charlie Wilson' 'Josh Baskin' 'James Donovan' 'Andrew Beckett' 'Larry Crowne' 'David S. Pumpkins' 'Michael Sullivan' 'Richard Phillips' 'Joe Banks' 'James Mattingly' 'Carl Hanratty')
 
 # Génération des insertions SQL
 for (( i=1; i<=4000000; i++ )); do
     code_postal="${codes_postaux[$RANDOM % ${#codes_postaux[@]}]}"
-    pays="${pays[$RANDOM % ${#pays[@]}]}"
+    pays="${listePays[$RANDOM % ${#listePays[@]}]}"
     destinataire="${noms_destinataires[$RANDOM % ${#noms_destinataires[@]}]}"
     expediteur="${noms_expediteurs[$RANDOM % ${#noms_expediteurs[@]}]}"
     contenu="Contenu du courrier numéro $i"
 
-    echo "INSERT INTO courrier (code_postal, ville, pays, destinataire, expediteur, contenu) VALUES ('$code_postal', 'Philadelphie', '$pays', '$destinataire', '$expediteur', '$contenu');"
+    echo "INSERT INTO courrier (code_postal, ville, pays, destinataire, expediteur, contenu) VALUES ('$code_postal', 'Philadelphie', '$pays', '$destinataire', '$expediteur', '$contenu');" >> data.sql
 done
